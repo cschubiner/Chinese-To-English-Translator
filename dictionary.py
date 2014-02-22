ba99e4f11f9a9964c4fb9467036d0959d032faeb
Clay Schubiner <cschubiner@gmail.com>
2014-02-22 15:41:30 -0800

    put in sentences

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+dictionary = dict()
+def getdictionary():
+  return dictionary
+
+dictionary['从'] = ['from', 'secondary', 'lax' 'unhurried', 'accessory', 'ever', 'obey', 'follow', 'observe', 'be engaged', 'second cousin', 'follower']
+
