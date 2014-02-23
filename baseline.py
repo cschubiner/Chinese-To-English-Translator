1e954cba378282ff14b8d80e4b63195eb9b4487f
Clay Schubiner <cschubiner@gmail.com>
2014-02-22 16:44:59 -0800

    dictionary

---
+++ baseline.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+import dictionary
+
+def translateSentence(sentence):
+  pass
+
+for line in open('corpus_dev.txt', 'r').readlines():
+  translateSentence(line)
--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
+def only_roman_chars(s):
+    try:
+        s.encode("iso-8859-1")
+        return True
+    except UnicodeDecodeError:
+        return False
+
 dictionary = dict()
 def getdictionary():
   return dictionary
 
-dictionary['从'] = ['from', 'secondary', 'lax' 'unhurried', 'accessory', 'ever', 'obey', 'follow', 'observe', 'be engaged', 'second cousin', 'follower']
+class EnglishWord:
+  def __init__(self, word, pos):
+    self.word = word
+    self.pos = pos
+  def addTarget(self, swissprot_id, swissprot_name):
+    self.proteinSet.add((swissprot_id, swissprot_name))
+
+char = ''
+pos = ''
+partsOfSpeech = set(['verb', 'noun', 'adverb', 'preposition', 'adjective', 'conjunction', 'number'])
+for line in open('raw_dictionary.txt', 'r').readlines():
+  line = line.strip()
+  if '-' in line and line.split('-')[1].strip() in partsOfSpeech:
+    pos = line.split('-')[1].strip()
+    char = line.split('-')[0].strip()
+    if char not in dictionary:
+      dictionary[char] = list()
+  elif len(line) > 1 and only_roman_chars(line):
+    dictionary[char].append(EnglishWord(line, pos))
+
+for engword in dictionary['日']:
+  print engword.word, engword.pos
 
