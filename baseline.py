07b83ebf05bcc920b12b644d535f115d789d2b8d
Clay Schubiner <cschubiner@gmail.com>
2014-02-22 17:37:21 -0800

    baseline sentence 1

--- baseline.py
+++ baseline.py
@@ -8,28 +8,19 @@ import codecs
 dictionary = dictionary.getdictionary()
 
 def translateSentence(sentence):
-  for i in range(0, len(sentence) - 2):
-    word = sentence[i:i+3]
-    # print word
-    if word in dictionary:
-      print dictionary[word][0].word
+  ret = ''
+  for char in sentence:
+    char = char.encode('utf-8')
+    if char in dictionary:
+      ret += dictionary[char][0].word
+      ret += ' '
     else:
-      pass
-      # print repr(word)
-      # print word.encode('utf-8')
+      ret += char
+  return ret
 
 
 # print dictionary['从'][0].word
 
 for line in codecs.open('corpus_dev.txt', encoding='utf-8').readlines():
-  # print line.encode('utf-8')
-  for char in line:
-    print char.encode('utf-8')
-  for char in repr(line).split('\\'):
-    # print eval(repr(char))
-    # print ('\\' + char).encode('utf-8')
-    pass
-  translateSentence(line)
-  # print line
-  # print line[:3]
-  break
+  # line = line.encode('utf-8')
+  print translateSentence(line)
