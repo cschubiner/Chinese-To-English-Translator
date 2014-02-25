0364ce8f9cd867191f41beeaf7f78f0ce849b925
Evan Shieh <eshieh@stanford.edu>
2014-02-24 20:50:22 -0800

    Modified slight POSTagger bug

--- features_test.py
+++ features_test.py
@@ -3,10 +3,10 @@
 
 import numpy
 import nltk
-from nltk.tag.stanford import NERTagger
+from nltk.tag.stanford import POSTagger
 
 ## Configure this to be your Java directory
-#nltk.internals.config_java(u"C:/Program Files/Java/jre7/bin/java.exe")
+nltk.internals.config_java(u"C:/Program Files/Java/jre7/bin/java.exe")
 
 chunk = u"妈妈带我去公园散步"
 #chunk = u"妈我"
@@ -17,7 +17,7 @@ chunk = u"妈妈带我去公园散步"
 #   print token,tag
 
 text = nltk.word_tokenize(chunk)
-st = NERTagger('chinese-distsim.tagger', 'stanford-postagger-3.1.4.jar')
+st = POSTagger('chinese-distsim.tagger', 'stanford-postagger-3.1.4.jar')
 poop = st.tag(text)
 print poop
 #tagger = pickle.load(open('sinica_treebank_brill_aubt.pickle'))
