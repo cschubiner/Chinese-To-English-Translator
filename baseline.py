51f42216b621a9f0a5bc0ec6f5d5adf281c63120
Clay Schubiner <cschubiner@gmail.com>
2014-02-24 21:45:37 -0800

    baseline translations

--- baseline.py
+++ baseline.py
@@ -7,6 +7,7 @@ import codecs
 chinDict = dictionary.getdictionary()
 
 def translateSentence(sentence):
+  sentence = sentence.split()
   ret = ''
   for word in sentence:
     if word in chinDict:
@@ -16,10 +17,20 @@ def translateSentence(sentence):
       ret += word
   return ret
 
+def translateSentenceNoSegmentation(sentence):
+  ret = ''
+  for char in sentence:
+    if char in chinDict:
+      ret += chinDict[char][0].word #use the most frequent translation
+      ret += ' '
+    else:
+      ret += char
+  return ret
+
 
 i = 0
 for line in codecs.open('corpus_dev_segmented.txt', encoding='utf-8').readlines():
   i += 1
-  print(str(i) + '.', translateSentence(line.split()))
+  print(str(i) + '.', translateSentence(line))
   # translateSentence(line)
   # break
