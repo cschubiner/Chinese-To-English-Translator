9e1787c8dcd2711fe759e3b173456f4599065828
Yitao Zhang <yitaoz@stanford.edu>
2014-02-24 22:02:49 -0800

    tense converter added

---
+++ tense.py
@@ -0,0 +1,10 @@
+import en
+
+print "Printing past tense of give"
+print en.verb.past("give")
+
+print "Printing plural of child"
+print en.noun.plural("child")
+
+print "Printing 100 as text"
+print en.number.spoken(100)
