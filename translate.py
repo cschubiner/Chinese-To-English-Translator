576e3ff434e6be1b12a756bd20fbbe876bf8bb2a
Evan Shieh <eshieh@stanford.edu>
2014-02-25 17:24:20 -0800

    Integrated ngram function

--- translate.py
+++ translate.py
@@ -4,10 +4,18 @@ import codecs
 import sys
 import re
 import copy
+from Datum import Datum
+from Sentence import Sentence
+from HolbrookCorpus import HolbrookCorpus
+from StupidBackoffLanguageModel import StupidBackoffLanguageModel
 
 chinDict = dictionary.getdictionary()
 
 replaceDictionary = {'、':',', '：':':', '；':';', '，':',', '％':'%', '。':'.', '？':'?'}
+
+engCorpus = HolbrookCorpus('holbrook-tagged-train.dat')
+engModel = StupidBackoffLanguageModel(engCorpus)
+
 def replaceChinesePunctuation(sentence):
   for key in replaceDictionary:
     sentence = sentence.replace(key, replaceDictionary[key])
@@ -64,7 +72,29 @@ def translateSentence(chineseSentence):
   newSentence = fixQuotes(newSentence)
   newSentence = fixPunctuationSpacing(newSentence)
   newSentence = fixDates(newSentence)
+
+  possibleSentences = []
+  possibleSentences.append(newSentence)
+  newSentence = chooseMostLikelySentence(possibleSentences)
   return newSentence
+
+# Uses the English language model (Stupid Backoff Bigram Model)
+# to choose a most "likely" sentence among several alternatives.
+# The input param "sentences" is a list of strings
+def chooseMostLikelySentence(sentences):
+  maxLogProbScore = float('-inf')
+  result = ""
+
+  # "sentence" is a string (assume delimited by spaces)
+  for sentence in sentences:
+    logProbScore = engModel.score(sentence.split())
+    if logProbScore > maxLogProbScore:
+      maxLogProbScore = logProbScore
+      result = sentence
+
+  # Return the most likely sentence
+  return result
+
 
 if __name__ == "__main__":
   i = 0
